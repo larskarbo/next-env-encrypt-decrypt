c4746341877b168b7c4ce07423197c9bc8448bb7
Lars Karbo <mail@larskarbo.no>
2022-06-24 13:18:19 +0200

    try without outputFileTracing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracing: true,
+  // outputFileTracing: true,
 };
 
 module.exports = nextConfig;
